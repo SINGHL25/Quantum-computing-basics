ef8b7c98abe304e45bd2496db91d1662e364f8bb
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:37:59 +1000

    Update grover_search.py

--- use_cases/grover_search.py
+++ use_cases/grover_search.py
@@ -1,1 +1,15 @@
 
+from qiskit import QuantumCircuit, Aer, execute
+
+def run_grover():
+    qc = QuantumCircuit(2, 2)
+    qc.h([0, 1])
+    qc.cz(0, 1)
+    qc.h([0, 1])
+    qc.measure([0, 1], [0, 1])
+
+    simulator = Aer.get_backend("qasm_simulator")
+    result = execute(qc, simulator, shots=1000).result()
+    counts = result.get_counts()
+
+    print("Grover Search Result:", counts)
