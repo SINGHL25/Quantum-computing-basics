5cb2224fc63fd05db354113cb7d3867c6066b0ea
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:32:24 +1000

    Update circuits.py

--- src/circuits.py
+++ src/circuits.py
@@ -1,1 +1,19 @@
 
+from qiskit import QuantumCircuit, Aer, transpile, execute
+from qiskit.visualization import plot_histogram
+import matplotlib.pyplot as plt
+
+def demo_qubit():
+    qc = QuantumCircuit(1,1)
+    qc.h(0)         # Hadamard gate: put qubit in superposition
+    qc.measure(0,0)
+    
+    simulator = Aer.get_backend('qasm_simulator')
+    job = execute(qc, simulator, shots=1000)
+    result = job.result()
+    counts = result.get_counts()
+    
+    print("Measurement results:", counts)
+    plot_histogram(counts)
+    plt.savefig("results/plots/measurement_histogram.png")
+    plt.show()
