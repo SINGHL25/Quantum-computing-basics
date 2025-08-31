b90b99f4b1549947ca7312ffa271fa3886a5d3b3
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:38:10 +1000

    Update quantum_fourier_transform.py

--- use_cases/quantum_fourier_transform.py
+++ use_cases/quantum_fourier_transform.py
@@ -1,1 +1,10 @@
 
+from qiskit import QuantumCircuit
+from qiskit.circuit.library import QFT
+
+def run_qft():
+    qc = QuantumCircuit(3)
+    qft = QFT(3)
+    qc.append(qft, [0,1,2])
+    print("Quantum Fourier Transform Circuit:")
+    print(qc)
