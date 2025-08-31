830a5dbbd09c97eca4a86c73c2fb24d14e0bc398
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:37:27 +1000

    Update entanglement.py

--- basics/entanglement.py
+++ basics/entanglement.py
@@ -1,1 +1,14 @@
 
+from qiskit import QuantumCircuit, Aer, execute
+
+def demo_entanglement():
+    qc = QuantumCircuit(2, 2)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.measure([0,1],[0,1])
+
+    simulator = Aer.get_backend("qasm_simulator")
+    result = execute(qc, simulator, shots=1000).result()
+    counts = result.get_counts()
+
+    print("Entanglement Result:", counts)
