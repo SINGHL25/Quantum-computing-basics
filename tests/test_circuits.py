ca4449a8828006d859d1ae595b7ea71faa1e1ba4
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:44:20 +1000

    Update test_circuits.py

--- tests/test_circuits.py
+++ tests/test_circuits.py
@@ -1,1 +1,10 @@
 
+from src import circuits
+
+def test_qubit_runs():
+    circuits.demo_single_qubit(shots=100)
+    assert True
+
+def test_bell_runs():
+    circuits.demo_bell_entanglement(shots=100)
+    assert True
