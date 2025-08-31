95b7780c779b5b3183c9825a565a7934d0c14f12
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:43:25 +1000

    Update simulators.py

--- src/simulators.py
+++ src/simulators.py
@@ -1,1 +1,16 @@
 
+from qiskit import Aer, transpile
+from qiskit.providers.backend import Backend
+from qiskit.result import Result
+
+def get_qasm_simulator() -> Backend:
+    return Aer.get_backend("qasm_simulator")
+
+def get_statevector_simulator() -> Backend:
+    return Aer.get_backend("statevector_simulator")
+
+def run(circuit, backend=None, shots=1024) -> Result:
+    backend = backend or get_qasm_simulator()
+    tcirc = transpile(circuit, backend)
+    job = backend.run(tcirc, shots=shots)
+    return job.result()
