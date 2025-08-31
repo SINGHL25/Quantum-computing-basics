6c70eeee935f801af3ec3c859bc944d799cd384e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:37:50 +1000

    Update quantum_teleportation.py

--- use_cases/quantum_teleportation.py
+++ use_cases/quantum_teleportation.py
@@ -1,1 +1,19 @@
 
+from qiskit import QuantumCircuit, Aer, execute
+
+def run_teleportation():
+    qc = QuantumCircuit(3, 3)
+    qc.h(1)
+    qc.cx(1, 2)
+    qc.cx(0, 1)
+    qc.h(0)
+    qc.measure([0,1], [0,1])
+    qc.cx(1, 2)
+    qc.cz(0, 2)
+    qc.measure(2, 2)
+
+    simulator = Aer.get_backend("qasm_simulator")
+    result = execute(qc, simulator, shots=1000).result()
+    counts = result.get_counts()
+
+    print("Teleportation Result:", counts)
