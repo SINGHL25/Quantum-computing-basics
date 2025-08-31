518d9791ec9036d54dac6f25bc7635e156fe7d8e
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:37:17 +1000

    Update superposition.py

--- basics/superposition.py
+++ basics/superposition.py
@@ -1,1 +1,13 @@
 
+from qiskit import QuantumCircuit, Aer, execute
+
+def demo_superposition():
+    qc = QuantumCircuit(1, 1)
+    qc.h(0)
+    qc.measure(0, 0)
+
+    simulator = Aer.get_backend("qasm_simulator")
+    result = execute(qc, simulator, shots=1000).result()
+    counts = result.get_counts()
+
+    print("Superposition Result:", counts)
