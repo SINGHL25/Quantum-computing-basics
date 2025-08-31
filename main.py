9c0c165955e7bc052ab22817f86170c3b68d9b60
Akhilesh singh <akhi.singh1989@gmail.com>
2025-08-31 19:32:06 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import argparse
+from src import circuits, simulators, visualizer
+
+def run_demo(demo):
+    if demo == "qubit":
+        circuits.demo_qubit()
+    elif demo == "grover":
+        circuits.demo_grover()
+    elif demo == "shor":
+        circuits.demo_shor()
+    else:
+        print("Demo not found!")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Quantum Computing Demos")
+    parser.add_argument("--demo", type=str, required=True,
+                        help="Choose demo: qubit | grover | shor")
+    args = parser.parse_args()
+    run_demo(args.demo)
 
